31a0791b80682a126b29f268f0c422e359e4b386
Seunghun Lee <waydi1@gmail.com>
2017-07-11 21:19:19 +0900

    Add Simple Validation Example

---
+++ RxSwiftPractice/SimpleValidation/SimpleValidationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleValidationViewController.swift
+//  RxSwiftPractice
+//
+//  Created by Seunghun Lee on 2017-07-11.
+//  Copyright © 2017 Seunghun Lee. All rights reserved.
+//
+
+import UIKit
+
+class SimpleValidationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
